1be5d314f48965d8366bdf90722b5ce9c0b9b220
Alexandre Labsi <alex.lbs2001@gmail.com>
2021-06-28 07:52:09 +0000

    add dashboard threshold

---
+++ controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BWB\Framework\mvc\controllers;
+
+use BWB\Framework\mvc\Controller;
+use BWB\Framework\mvc\models\DefaultModel;
+use BWB\Framework\mvc\models\TestModel;
+use BWB\Framework\mvc\dao\DAOProduct;
+use Exception;
+
+class DashboardController extends Controller {
+
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function renderDashboard(){
+
+        $data = array(
+            "prodsThresh" => (new DAOProduct)->getProductThresh()
+        );
+        $this->render("dashboard", $data);
+    }
+}
